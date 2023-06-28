8f8e5cafad132f690c9afca0e105544742650ec4
TeodorGruia <sjtg105@gmail.com>
2023-06-28 17:00:43 -0400

    Edited program flow

--- functions.py
+++ functions.py
@@ -14,10 +14,10 @@ def menu():
 def get_miles(total_purchases, type="d"):
     if type == "d" or type == "h":
         miles_earned = total_purchases * 3
-        return miles_earned, "via Delta or hotel purchases"
+        print(f"You have {miles_earned} miles via Hotels or Delta purchases")
     elif type == "r" or type == "s":
         miles_earned = total_purchases * 2
-        return miles_earned, " via resturants amd groceries"
+        print(f"You have {miles_earned} miles via Resturants or Groceries")
     else:
         miles_earned = total_purchases
-        return miles_earned, "via other purchases"
+        print(f"You have {miles_earned} miles via other purchases") 
